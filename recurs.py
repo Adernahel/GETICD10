13f5f21b710fdedd119a96d2216004af1d2ea294
Adernahel <61577580+Adernahel@users.noreply.github.com>
2020-08-17 14:18:08 +0200

    Create recurs.py

---
+++ recurs.py
@@ -0,0 +1,29 @@
+from urllib.request import Request, urlopen
+import json
+import csv
+
+import pandas as pd    
+import numpy as np
+
+url = 'https://api.dane.gov.pl/resources/10566,miedzynarodowa-statystyczna-klasyfikacja-chorob-i-problemow-zdrowotnych-icd10-eng-/data'
+
+
+def recur(urlt):
+    request = Request(urlt)
+    response = urlopen(request)
+    icd10 = response.read()
+    selflink = json.loads(icd10)['links']['self']
+    nextlink = json.loads(icd10)['links']['next']
+    lastlink = json.loads(icd10)['links']['last']
+    print(nextlink)
+    if selflink != lastlink:
+        return(recur(nextlink))
+    else:
+        return None
+
+
+    
+
+
+
+

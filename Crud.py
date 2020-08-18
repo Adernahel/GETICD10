9a108991d79e606c660d68fe5df85c5e59a56f5c
Bartosz Lipiński <barosz.lipinski@gmail.com>
2020-08-18 11:21:10 +0200

    commit

--- Crud.py
+++ Crud.py
@@ -5,6 +5,7 @@ from Main import PatientRandom
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import date
+from ICD10csv import get_icd10_csv
 
 engine = create_engine(DATABASE_URI,echo=True)
 """
@@ -17,7 +18,7 @@ recreate_database()
 Session = sessionmaker(bind=engine)
 session = Session()
 
-def AddNewPatients(amount):
+def Add_New_Patients(amount):
     patient_list = []
     
     for i in range(amount):
@@ -36,6 +37,16 @@ def AddNewPatients(amount):
         session.add(patient)
         session.commit()
         session.close
+
+def Populate_ICD10_Table(icd10_data_frame):
+    df = icd10_data_frame
+    df.columns = ['ICD10','Name']
+    df.to_sql('diagnosisdict',con=engine, if_exists='append',index_label='id')
+    engine.execute("select * from diagnosisdict").fetchall()
+
+
+Populate_ICD10_Table(get_icd10_csv())
+            
 
 
 
--- ICD10csv.py
+++ ICD10csv.py
@@ -3,15 +3,12 @@ import os
 import pandas as pd
 
 
+def get_icd10_csv():
 
-path_to_file = os.getcwd() + '\\' + 'ClinicDB\\ICD10en.csv'
-
-
-
-def get_icd10_csv(csv_path):
+    path_to_file = os.getcwd() + '\\' + 'ClinicDB\\ICD10en.csv'
 
     icd10_csv = pd.read_csv(path_to_file, sep=";",encoding="ANSI")
-    
+
     icd10_data_frame_csv = pd.DataFrame(icd10_csv)
 
     icd10_data_frame_csv = icd10_data_frame_csv[["Subcategory code","subcategory"]]
@@ -20,4 +17,4 @@ def get_icd10_csv(csv_path):
 
     return icd10_data_frame_csv.drop_duplicates()
 
-print(get_icd10_csv(path_to_file))
+
